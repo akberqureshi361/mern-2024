68bf3212d4dd4f3f6f8dff486d2332c0c0927fd3
ahad <akberqureshi361@gmail.com>
2023-12-29 16:24:11 +0530

    backend connection with mongodb and Securing Your Private Data with Dotenv in our Backend App

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 const express = require('express');
 const router = require('./router/auth.router');
 const app = express();
+const connectdb = require("./utils/db");
+
 
 app.use(express.json());
 
@@ -9,7 +12,9 @@ app.use("/api/auth", router);
 
 
 const PORT = 8000;
+connectdb().then (() => {
 
-app.listen(8000, () =>{
-    console.log("my name is khan ");
-})
+    app.listen(8000, () =>{
+        console.log("my name is khan ");
+    });
+});
---
+++ utils/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const URI = process.env.MONGODB_URI;
+
+const Connecteddb = async () => {
+try {
+    mongoose.connect(URI);
+    console.log("connected succes mera kdfjkd ");
+} catch (error) {
+    console.error("database connection failed");
+    process.exit(0);
+}
+};
+module.exports = Connecteddb;
