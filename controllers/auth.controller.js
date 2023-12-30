816b234d502fab9d53a4f897ce2a4031db8b4699
ahad <akberqureshi361@gmail.com>
2023-12-30 13:37:36 +0530

    changing

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,8 @@
+const User = require("../models/user-models");
+
+
+
+
 const home = async (req, res) => {
     try {
         res.send("welcome to world best mern series by ahad qureshi  using router");
@@ -5,10 +10,33 @@ const home = async (req, res) => {
         console.log("error");
     }
 };
+// user Registration Logic 
+// 1. Get Registration data : Retrive user data (username, email , password );
+// 2. Check email exitence : Check if the email is already registred;
+// 3. Hash Password : Securely has the password ;
+// 4. Create User : Create a new user with hashed  passoword .
+// 5. Save to DB : Save usdrdata to database ,
+// 6. Respond : Respond with "Registration Succesfull " or handle errors .
+
+
+
+
 
 const register = async (req, res) => {
+
+
     try {
-        res.json({ message: req.body });
+        console.log(req.body);
+        const { username, email, phone, password } = req.body;
+
+        const userExist = await User.findOne({ email });
+        if (userExist) {
+            return res.status(400).json({ msg: "email already exists" });
+        }
+        const userCreate = await User.create({ username, email, phone, password });
+
+
+        res.status(200).json({ msg: userCreate });
 
     } catch (error) {
         res.json({ msg: "page not found " })
--- utils/db.js
+++ utils/db.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 const URI = process.env.MONGODB_URI;
 
 const Connecteddb = async () => {
-try {
-    mongoose.connect(URI);
-    console.log("connected succes mera kdfjkd ");
-} catch (error) {
-    console.error("database connection failed");
-    process.exit(0);
-}
+    try {
+        mongoose.connect(URI);
+        console.log("connected succes mera kdfjkd ");
+    } catch (error) {
+        console.error("database connection failed");
+        process.exit(0);
+    }
 };
 module.exports = Connecteddb;
