627ca0b1eeef0ed7cc4e2fb3e427be135f1f71ee
ahad <akberqureshi361@gmail.com>
2023-12-30 12:36:03 +0530

    Storing Registered User Data in an Online Database with Express and Mongoose

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,6 @@
 const home = async (req, res) => {
     try {
-        res.send("welcome to world best mern series by ahad using router");
+        res.send("welcome to world best mern series by ahad qureshi  using router");
     } catch (error) {
         console.log("error");
     }
