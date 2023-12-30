3eb9cfcd526cd1a11b5e58dfd1e4bb5d96b6bbf6
ahad <akberqureshi361@gmail.com>
2023-12-30 12:28:14 +0530

    few changign

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,18 +1,18 @@
-const home = async ( req, res ) => {
-    try{
+const home = async (req, res) => {
+    try {
         res.send("welcome to world best mern series by ahad using router");
     } catch (error) {
-            console.log("error");
+        console.log("error");
     }
 };
 
-const register = async ( req, res ) => {
-try {
-    res.json({message: req.body});
+const register = async (req, res) => {
+    try {
+        res.json({ message: req.body });
 
-} catch (error) {
-    res.json({msg:"page not found "})
-}
+    } catch (error) {
+        res.json({ msg: "page not found " })
+    }
 };
 
 module.exports = { home, register };
