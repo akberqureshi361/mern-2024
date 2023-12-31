7a6f38d191af59d1cf3ffc80bb8d4d0b9e1f4d90
ahad <akberqureshi361@gmail.com>
2023-12-31 18:38:33 +0530

    Building a Complete User Login Route with Functionality in Our MERN App

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -52,9 +52,48 @@ const register = async (req, res) => {
             userId: userCreated._id.toString(),
         });
 
+    } catch (error) {
+        res.status(500).json({ msg: "page not found " });
+    }
+};
+// user Login Logic start 
+const login = async (req, res) => {
+
+    try {
+        const { email, password } = req.body;
+        const userExist = await User.findOne({ email });
+        console.log(userExist);
+
+        if (!userExist) {
+            return res.status(400).json({ msg: "Invalid Credentials" });
+        }
+        const isPasswordValid = await bcrypt.compare(password, userExist.password);
+
+        console.log(userExist.password);
+
+        if(isPasswordValid) {
+            res.status(200).json({
+                msg: "Login succesfull",
+                token: await userExist.generateToken(),
+                userId: userExist._id.toString(),
+                
+            });
+        } else {
+            res.status(401).json({ message: "Invalid email or password " })
+        }
+
     }catch (error) {
         res.status(500).json({ msg: "page not found " });
-    };
+    }
+    
 };
 
-module.exports = { home, register };
+
+
+// user Login Logic end
+
+
+
+
+
+module.exports = { home, register, login };
