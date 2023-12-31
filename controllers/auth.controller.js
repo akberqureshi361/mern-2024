6c8978e7ca5940726b340b73ed2ca938ffa09c5a
ahad <akberqureshi361@gmail.com>
2023-12-31 13:36:09 +0530

    changing completed

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -30,27 +30,31 @@ const register = async (req, res) => {
         console.log(req.body);
         const { username, email, phone, password } = req.body;
 
-        const userExist = await User.findOne({ email : email });
+        const userExist = await User.findOne({ email });
 
         if (userExist) {
             return res.status(400).json({ msg: "email already exists" });
         }
 
-// hash the password  
-                const saltRound = 10;
-                const hash_password = await bcrypt.hash( password, saltRound);
+        // hash the password  
+        const saltRound = 10;
+        const hash_password = await bcrypt.hash(password, saltRound);
 
 
 
 
-        const userCreated = await User.create({ username, email, phone, password:hash_password, });
+        const userCreated = await User.create ({ username, email, phone, password: hash_password, });
 
 
-        res.status(200).json({ msg: userCreated });
+        res.status(201).json({
+            msg: "registration succesfull",
+            token: await userCreated.generateToken(),
+            userId: userCreated._id.toString(),
+        });
 
-    }catch (error) {
+    } catch (error) {
         res.status(500).json({ msg: "page not found " });
-    }
+    };
 };
 
 module.exports = { home, register };
--- models/user-models.js
+++ models/user-models.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -35,10 +36,41 @@ const userSchema = new mongoose.Schema({
 //            const hash_password = await bcrypt.has(user.password,saltRound);
 //            user.password = hash_password; 
 //         } catch (error) {
-            
+
 //         }
 // });
 // const bcrypt = require("bcryptjs");  yaad rakhe ye aapko import karna hoga upar 
+
+
+
+// json web token (jwt token )
+// jwt ko hum kabhi bhi server p store nahi karte hain blaki hum ise apne server p hi karte hain
+// or hum jab apne browser p data ko save karte  hain use (cookie) kaha jata h !!yaad rakhan
+//  ye bahut important h yaad rakhe 
+
+userSchema.methods.generateToken = async function () {
+
+    try {
+
+        return jwt.sign({
+            userId: this._id.toString(),
+            email: this.email,
+            isAdmin: this.isAdmin,
+
+
+        }, process.env.JWT_SECRET_KEY,
+            {
+                expiresIn: "30d",
+            }
+        )
+    } catch (error) {
+
+    }
+};
+
+
+
+
 
 
 
